f90c5feb8a9be57595e00a66115d6128259dda8a
Gabriel <gabst138@gmail.com>
2021-07-29 19:38:59 -0300

    Create exe03.php

---
+++ aula13 - FOR/exe03.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estilo.css">
+    <title>Números primos</title>
+</head>
+<body>
+<div>
+    <form method="get" action="exe03.php">
+        Número: 
+        <input type="number" name="num" min="0" max="100" value="1">
+        <input type="submit" name="verificar...">
+    </form>
+    <?php
+        $num = isset($_GET["num"])?$_GET["num"]:1;
+        $res1 = $num / 1;
+        $res = $num / $num;
+        if ($res1 && $res == 0){
+            echo "$num é Primo";
+        }
+        echo "$res1 $res"
+    ?>
+</div>
+    
+</body>
+</html>
