4fd37cc74fc7c7c319c18327dc41bde910034bfa
Gabriel <gabst138@gmail.com>
2021-08-26 19:48:52 -0300

    update

---
+++ POO/class/ClassUfc.php
@@ -0,0 +1,64 @@
+<?php 
+  class Lutador {
+    //Atributos
+    private $nome;
+    private $nacionalidade;
+    private $idade;
+    private $altura;
+    private $peso;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
+
+    // Métodos (funções)
+    public function apresentar() {
+      
+    }
+    public function status() {
+      
+    }
+    public function ganharLuta() {
+      
+    }
+    public function perderLuta() {
+      
+    }
+    public function empatarLuta() {
+      
+    }
+
+    // Método construtor
+
+    // Métodos getters
+    private function getNome() {
+
+    }
+    private function getNacionalidade() {
+      
+    }
+    private function getIdade() {
+      
+    }
+    private function getAltura() {
+      
+    }
+    private function getPeso() {
+      
+    }
+    private function getCategoria() {
+      
+    }
+    private function getVitorias() {
+      
+    }
+    private function getDerrotas() {
+      
+    }
+    private function getEmpates() {
+      
+    }
+    // Métodos setters
+
+
+  }
