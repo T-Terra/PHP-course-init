7be90bf496b1f319296a224a23fb2052b73a4b38
Gabriel <gabst138@gmail.com>
2021-08-25 23:07:02 -0300

    update exe controllers

--- POO/aula06b-p/main.php
+++ POO/aula06b-p/main.php
@@ -10,8 +10,14 @@
   <pre>
     <?php
       require_once "../class/ClassControllers.php";
-      /*$con1 = new ControleRemoto;
-      print_r($con1);*/
+      // Objeto
+      $con1 = new ControleRemoto;
+      $con1->ligar();
+      //$con1->maisVolume();
+      $con1->menosVolume();
+      $con1->abrirMenu();
+      //$con1->desligar();
+      //print_r($con1);
     ?>
   </pre>
   
--- POO/class/ClassControllers.php
+++ POO/class/ClassControllers.php
@@ -24,6 +24,7 @@
       $this->tocando = $t;
     }
 
+
     // Métodos getters
     private function getVolume(){
       return $this->volume;
@@ -34,5 +35,65 @@
     private function getTocando(){
       return $this->tocando;
     }
+
+    // Implementação dos métodos da interface
+      public function ligar() {
+        $this->setLigado(true);
+        if ($this->getLigado() === true && $this->getTocando() === false) {
+          echo "<p>Tv ligada!!!</p>";
+        }
+      }
+      public function desligar() {
+        $this->setLigado(false);
+        if ($this->getLigado() === true && $this->getTocando() === true) {
+          echo "<p>Tv Desligada!!!</p>";
+        }
+      }
+      public function abrirMenu() {
+        echo "<br>-----MENU----";
+        echo "<br>Está ligado?: ". ($this->getLigado()?"SIM":"NÃO");
+        echo "<br>Está tocando?: ". ($this->getTocando()?"SIM":"NÃO");
+        echo "<br>Volume: ". $this->getVolume();
+        for ($i=0; $i<= $this->getVolume(); $i+=10) {
+          echo "|";
+        }
+        echo "<br>";
+      }
+      public function fecharMenu() {
+        echo "<p>O Menu foi fechado!</p>";
+      
+      }
+      public function maisVolume() {
+        if ($this->getLigado() === true) {
+          $this->setVolume($this->getVolume() + 10);
+        }
+      }
+      public function menosVolume() {
+        if ($this->getLigado() === true) {
+          $this->setVolume($this->getVolume() - 10);
+        }
+      }
+      public function ligarMudo() {
+        if ($this->getLigado() === true && $this->getVolume() > 0) {
+          $this->setVolume(0);
+        }
+      }
+      public function desligarMudo() {
+        if ($this->getLigado() === true && $this->getVolume() === 0) {
+          $this->setVolume(50);
+        }
+      }
+      public function play() {
+        if ($this->getLigado() === true && $this->getTocando() === false) {
+          $this->setTocando(true);
+          echo "<p>Tocando!!!>>></p>";
+        }
+      }
+      public function pause() {
+        if ($this->getLigado() === true && $this->getTocando() === true) {
+          $this->setTocando(false);
+          echo "<p>Pausado!!!!>>></p>";
+        }
+      }
 
   }
