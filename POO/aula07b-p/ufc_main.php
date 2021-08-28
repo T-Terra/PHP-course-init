f566529b64d132e79768104fe17dcf308529c2bc
Gabriel <gabst138@gmail.com>
2021-08-28 15:19:08 -0300

    update

--- POO/aula07b-p/ufc_main.php
+++ POO/aula07b-p/ufc_main.php
@@ -10,9 +10,15 @@
   <pre>
     <?php 
       require_once "../class/ClassUfc.php";
-      $Lutador1 = new Lutador();
-      $Lutador1->apresentar();
-      //print_r($Lutador1);
+      $l[0] = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 3, 1);
+      $l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+      $l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+      $l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+      $l[4] = new Lutador("Gabriel", "Brasil", 21, 1.81, 82, 24, 0, 0);
+      $l[5] = new Lutador("Thomas", "Peru", 30, 1.90, 110, 10, 8, 3);
+      $l[4]->apresentar();
+      $l[5]->apresentar();
+      //print_r($l);
     ?>
   </pre>
   
--- POO/class/ClassUfc.php
+++ POO/class/ClassUfc.php
@@ -11,14 +11,26 @@
     private $derrotas;
     private $empates;
 
+    // Método construtor
+    public function __construct($no, $na, $ida, $al, $pe, $vi, $de, $em) {
+      $this->setNome($no);
+      $this->setNacionalidade($na);
+      $this->setIdade($ida);
+      $this->setAltura($al);
+      $this->setPeso($pe);
+      $this->setVitorias($vi);
+      $this->setDerrotas($de);
+      $this->setEmpates($em);
+    }
+    
     // Métodos (funções)
     public function apresentar() {
       echo "<p>---------LUTADOR {$this->getNome()}---------</p>";
       echo "Lutador: {$this->getNome()}";
       echo "<br>Nacionalidade: {$this->getNacionalidade()}";
-      echo "<br>Idade: {$this->getIdade()}";
-      echo "<br>Altura: {$this->getAltura()}";
-      echo "<br>Peso: {$this->getPeso()}";
+      echo "<br>Idade: {$this->getIdade()} anos";
+      echo "<br>Altura: {$this->getAltura()}m de altura";
+      echo "<br>Peso: {$this->getPeso()}Kg";
       echo "<br>Categoria: {$this->getCategoria()}";
       echo "<br>Vitórias: {$this->getVitorias()}";
       echo "<br>Derrotas: {$this->getDerrotas()}";
@@ -26,33 +38,26 @@
     }
 
     public function status() {
-      
+      echo "<p>-----STATUS-----</p>";
+      echo "{$this->getNome()}";
+      echo "<br>É um peso {$this->getCategoria()}";
+      echo "<br>Vitórias: {$this->getVitorias()}";
+      echo "<br>Derrotas: {$this->getDerrotas()}";
+      echo "<br>Empates: {$this->getEmpates()}";
     }
 
     public function ganharLuta() {
-      
+      $this->setVitorias($this->getVitorias() + 1);
     }
 
     public function perderLuta() {
-      
+      $this->setDerrotas($this->getDerrotas() + 1);
     }
 
     public function empatarLuta() {
-      
+      $this->setEmpates($this->getEmpates() + 1);
     }
 
-    // Método construtor
-    public function __construct() {
-      $this->setNome("Desconhecido...");
-      $this->setNacionalidade("Desconhecida...");
-      $this->setIdade(0);
-      $this->setAltura(0);
-      $this->setPeso(0);
-      $this->setCategoria("Desconhecida...");
-      $this->setVitorias(0);
-      $this->setDerrotas(0);
-      $this->setEmpates(0);
-    }
 
     // Métodos getters
     private function getNome() {
@@ -97,9 +102,21 @@
     }
     private function setPeso($pe) {
       $this->peso = $pe;
+      $this->setCategoria();
     }
-    private function setCategoria($cat) {
-      $this->categoria = $cat;
+    private function setCategoria() {
+      //$this->categoria = $cat;
+      if ($this->getPeso() <= 52.2){
+        $this->categoria = "INVÁLIDO!";
+      } elseif ($this->getPeso() <= 70.3) {
+        $this->categoria = "LEVE";
+      } elseif ($this->getPeso() <= 83.9) {
+        $this->categoria = "MÉDIO";
+      } elseif ($this->getPeso() <= 120.2) {
+        $this->categoria = "PESADO";
+      } else {
+        $this->categoria = "INVÁLIDO!";
+      }
     }
     private function setVitorias($vic) {
       $this->vitorias = $vic;
